edc7a820c8b468760687dd106d32af79deaec9ba
ckyyyy <chongkyxxx@gmail.com>
2020-07-15 15:02:37 +0800

    refactor make move api

--- game/lib/game.ex
+++ game/lib/game.ex
@@ -4,11 +4,11 @@ defmodule Game do
 
   #API module
   defdelegate new_game(), to: Hangman
-  defdelegate make_move(game, guess), to: Hangman
+  # defdelegate make_move(game, guess), to: Hangman
   defdelegate tally(game), to: Hangman
 
-#  def make_move(game, guess) do
-#    game = Game.make_move(game, guess)
-#    { game, tally(game) }
-#  end
+  def make_move(game, guess) do
+    game = Hangman.make_move(game, guess)
+    { game, tally(game) }
+  end
 end
--- game/lib/hangman/hangman.ex
+++ game/lib/hangman/hangman.ex
@@ -19,13 +19,13 @@ defmodule Game.Hangman do
   end
 
   def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
-    { game, tally(game) }
-    # game
+    # { game, tally(game) }
+    game
   end
 
   def make_move(game, guess) do
     game = accept_move(game, guess, MapSet.member?(game.guessed, guess))
-    { game, tally(game) }
+    # { game, tally(game) }
   end
 
   def tally(game) do
